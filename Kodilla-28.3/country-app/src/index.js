d07156c5fa0c8992fedcd4519a151a5753a2ad62
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-06 21:00:43 +0200

    add DevTools, edit actions, index, reducer 	modified:   package-lock.json 	modified:   package.json 	new file:   src/DevTools.js 	modified:   src/actions/actionTypes.js 	modified:   src/index.js 	modified:   src/reducers/countriesReducer.js

--- Kodilla-28.3/country-app/src/index.js
+++ Kodilla-28.3/country-app/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { getCountries } from './actions/actionsCountries'
+import DevTools from './DevTools';
 
 render(
-    <h1>Inicjalizacja projektu</h1>,
+    <Provider store={store}>
+        <div>
+            <h1>App</h1>
+            <DevTools />
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
