21a7c05b21414ef1ca797b84e1075c1b0f038051
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-07 17:25:47 +0200

    remove commented devtool

--- Kodilla-28.4/country-app/src/index.js
+++ Kodilla-28.4/country-app/src/index.js
@@ -4,15 +4,9 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
-
-//import { getCountries } from './actions/actionsCountries';
-//import DevTools from './DevTools';
 render(
   <Provider store={store}>
     <Router history={hashHistory} routes={routes} />
-    {/*<DevTools />*/}
   </Provider>,
   document.getElementById('root')
 );
-
-//store.dispatch(getCountries());
