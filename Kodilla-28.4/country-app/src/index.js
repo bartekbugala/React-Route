c5df007af642236099cf88f4415637fb24b0ee63
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-07 11:35:25 +0200

    add navigation + routing

--- Kodilla-28.4/country-app/src/index.js
+++ Kodilla-28.4/country-app/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import { getCountries } from './actions/actionsCountries'
-import DevTools from './DevTools';
-
+//import { getCountries } from './actions/actionsCountries';
+import Navigation from './presentational/navigation.component';
+//import DevTools from './DevTools';
 render(
-    <Provider store={store}>
-        <div>
-            <h1>App</h1>
-            <DevTools />
-        </div>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={Navigation}></Route>
+    </Router>
+
+    {/*<DevTools />*/}
+  </Provider>,
+  document.getElementById('root')
 );
 
-store.dispatch(getCountries());
+//store.dispatch(getCountries());
